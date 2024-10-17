43b28115277d9c8f5d054d07a3f26eab0f6aac67
Novel272 <mohammedalmustafak@gmail.com>
2024-10-17 02:59:05 +0000

    x

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,66 +1,37 @@
+
 #!/usr/bin/python3
-"""
-Impletion of a solution to the Minimum Operations problem
-Re to the README to read the problem's detailed explanation
-"""
+'''Coding challenge.
+'''
 
 
-def is_prime(n: int) -> bool:
-    """Rets prime or not"""
-
-    if n <= 1:
-        return False
-    elif n <= 3:
-        return True
-    elif n % 2 == 0 or n % 3 == 0:
-        return False
-
-    i = 5
-    while i ** 2 <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-
-    return True
-
-
-def minOperations(n: int) -> int:
-    """
-    Calculates the fewest number of operations needed
-    to result in exactly `n` H characters in the file.
-
-    Args:
-        n (int): The desired number of H characters in the file.
-
-    Returns:
-        int: The minimum number of operations required.
-        If n is impossible to achieve or no operations are needed, return 0.
-    """
-
-    # If n isnot int
-    if not type(n) is int:
+def minOperations(n):
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
 
-    # We can't acve n if n < 1
-    # And if n == 1, it's initial state so we do nothing
-    if n <= 1:
-        return 0
-
-    # If n is prime can only ge by pasting it n times
-    if is_prime(n):
-        return n
-
-    # Initialize Divisor Prime Factors Sum
-    divisor = 2
-    factors_sum = 0
-
-    
-    num = n
-    while num > 1:
-        while num % divisor == 0:
-            factors_sum += divisor
-            num //= divisor
-        divisor += 1
-
-    
-    return factors_sum
